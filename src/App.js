be28ee9251d56147a9363fc99abbfb906c702437
Kathan Yatin Parikh <kathanparikh0@gmail.com>
2025-11-27 15:59:44 -0500

    Fix import order for Vercel build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherCard from "./WeatherCard";
+import './App.css';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
@@ -55,4 +56,3 @@ function App() {
 }
 
 export default App;
-import './App.css';
